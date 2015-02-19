fea56cffd426e3b28df1fe973a18b7f4a2fdd5d1
Nate Irwin <nate@nateirwin.net>
2015-02-19 12:07:24 -0700

    Support passing date string in

--- index.js
+++ index.js
@@ -6,9 +6,17 @@
 
 var dateformat = require('dateformat');
 
-module.exports = function date(format) {
+module.exports = function date(str, format) {
+  var dt;
+
+  if (typeof str === 'string') {
+    dt = new Date(str);
+  } else {
+    dt = new Date();
+  }
+
   if (typeof format !== 'string' || format === 'today') {
     format = 'mmmm dd, yyyy';
   }
-  return dateformat(new Date(), format);
+  return dateformat(dt, format);
 };
